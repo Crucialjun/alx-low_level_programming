25a6fd3951fc160b583e17185bc2c03d5c19b274
crucialjun <nich.otieno@gmail.com>
2022-09-08 06:02:44 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-fprintf("Size of a char: %d bytes(s)\n", sizeof(char));
-fprintf("Size of an int: %d bytes(s)\n", sizeof(int));
-fprintf("Size of a long int: %d bytes(s)\n", sizeof(long int));
-fprintf("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
-fprintf("Size of a float: %d bytes(s)\n", sizeof(float));
+puts("Size of a char: %d bytes(s)\n", sizeof(char));
+puts("Size of an int: %d bytes(s)\n", sizeof(int));
+puts("Size of a long int: %d bytes(s)\n", sizeof(long int));
+puts("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
+puts("Size of a float: %d bytes(s)\n", sizeof(float));
 /* returns 0 */
 return (0);
 }
