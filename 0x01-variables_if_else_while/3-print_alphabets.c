653aac63398cf98cb6b392e9723652993d51f4e3
crucialjun <nich.otieno@gmail.com>
2022-09-16 09:00:16 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; ++c)
+{
+putchar(c)
+}
+for (c = 'A'; c <= 'Z'; ++c)
+{
+putchar(c)
+}
+
+printf("\n");
+/* returns 0 */
+return (0);
+}
