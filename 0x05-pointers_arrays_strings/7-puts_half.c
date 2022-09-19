73e1e2e132e07f0d3dac130cd7547ea156ff9072
crucialjun <nich.otieno@gmail.com>
2022-09-19 15:38:03 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts_half - check the code
+ *
+ * @str : char pointer
+ *
+ * Return: length of s.
+ */
+void puts_half(char *str)
+{
+int length,i,n;
+
+length = strlen(str);
+
+if (length % 2 != 0)
+{
+n = (length - 1) / 2;
+}
+else
+{
+n = length / 2;
+}
+for(i = n;i < length ; ++i)
+{
+putchar(str[i]);
+}
+}
