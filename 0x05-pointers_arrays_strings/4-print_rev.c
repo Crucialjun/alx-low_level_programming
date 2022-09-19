78970a4ad62b9be3cd9a68715a01725749a5183d
crucialjun <nich.otieno@gmail.com>
2022-09-19 11:40:19 -0700

    m

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,11 +13,10 @@ int length, temp, i;
 
 length = strlen(s);
 
-for (i = 0; i < length / 2; ++i)
+for (i = length; i > length; --i)
 {
-temp = s[i];
-s[i] = s[length - i - 1];
-s[length - i - 1] = temp;
+printf("%s\n", s[i]
 }
+return (0);
 }
 
