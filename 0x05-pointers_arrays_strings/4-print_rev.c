944c129503175005488047d011ee954bb4f8c8e9
crucialjun <nich.otieno@gmail.com>
2022-09-19 11:20:41 -0700

    m

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * print_rev - check the code
+ *
+ * @s : char pointer
+ *
+ * Return: length of s.
+ */
+void print_rev(char *s)
+{
+int length, temp;
+
+length = strlen(s)
+
+for(int i = 0;i < len/2;i++)
+{
+temp = s[i];
+s[i] = s[length - i -1];
+s[length - i -1] = temp;
+}
+
