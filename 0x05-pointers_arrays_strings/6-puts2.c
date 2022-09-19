10a2e234f7800939eb0148740510ef7677810a24
crucialjun <nich.otieno@gmail.com>
2022-09-19 15:36:44 -0700

    m

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * puts2 - check the code
+ * puts_half - check the code
  *
  * @str : char pointer
  *
  * Return: length of s.
  */
-void puts2(char *str)
+void puts_half(char *str)
 {
-int length, i;
+int length,i,n;
 
 length = strlen(str);
-for (i = 0; i < length; i = i + 2)
+
+if (length % 2 != 0)
+{
+n = (length - 1) / 2;
+}
+else
+{
+n = length / 2;
+}
+for(i = n;i < length ; ++i)
 {
 putchar(str[i]);
 }
-printf("\n");
 }
