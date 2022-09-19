5f275ed7dc9781b3a4037bf5d877bb384fb36119
crucialjun <nich.otieno@gmail.com>
2022-09-19 15:05:25 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts2 - check the code
+ *
+ * @str : char pointer
+ *
+ * Return: length of s.
+ */
+void puts2(char *str)
+{
+int length, i;
+
+length = strlen(str);
+for (i = 0; i < length; i = i + 2)
+{
+putchar(str[i]);
+}
+}
