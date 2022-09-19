7b357a7ee08e0d265072a14361fb4d0a9a681d13
crucialjun <nich.otieno@gmail.com>
2022-09-19 09:35:33 -0700

    m

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * _strlen - check the code
+ *
+ * @s : char pointer
+ *
+ * Return: length of s.
+ */
+int _strlen(char *s)
+{
+int i;
+
+for(i = 0; *s[i] != '\0'; ++i);
+
+return i
+}
+
