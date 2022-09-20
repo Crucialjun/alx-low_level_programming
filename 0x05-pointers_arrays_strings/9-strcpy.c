9fcc9e462ef3b0180446a6a9cb6f8e4afccf2531
crucialjun <nich.otieno@gmail.com>
2022-09-20 07:28:35 -0700

    m

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_array - check the code
+ *
+ * @dest : char pointer
+ * @src : char pointer
+ *
+ * Return: length of s.
+ */
+char *_strcpy(char *dest, char *src)
+{
+    strcpy(dest,src);
+}
