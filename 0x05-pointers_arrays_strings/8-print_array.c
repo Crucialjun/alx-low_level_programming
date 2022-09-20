a3043e6f13859ee512ad1ad5bbd098dc2d7cbf42
crucialjun <nich.otieno@gmail.com>
2022-09-20 06:44:35 -0700

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_array - check the code
+ *
+ * @a : char pointer
+ * @n : char pointer
+ *
+ * Return: length of s.
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; ++i)
+{
+printf("%d", a[i]);
+if (i < n - 1)
+{
+printf(", ");
+}
+}
+}
