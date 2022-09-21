7dba39103ae0bd89c5e2a715c14cb6aa2aa1c194
crucialjun <nich.otieno@gmail.com>
2022-09-21 02:12:18 -0700

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strncat - check the code
+ *
+ * @dest : char pointer
+ * @src : char pointer
+ * @n : int
+ *
+ * Return: length of s.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+char *ptr = dest + strlen(dest)
+while (*src != '\0' && n--)
+{
+*ptr++ = *src++;
+}
+*ptr = '\0';
+return (dest);
+}
