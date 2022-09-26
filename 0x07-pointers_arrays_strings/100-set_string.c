395c67f263fbf3054f437b46350ad19392c9bcbd
crucialjun <nich.otieno@gmail.com>
2022-09-26 13:44:23 -0700

    m

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *set_string - check the code
+ *
+ * @s : char
+ *
+ * @to : char
+ *
+ *
+ * Return:null
+ */
+void set_string(char **s,char *to)
+{
+*s = to;
+}
