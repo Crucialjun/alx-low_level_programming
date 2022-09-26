e64dd0068cbe3e1c169c88cad4db3ca3007a8bfa
crucialjun <nich.otieno@gmail.com>
2022-09-26 13:21:51 -0700

    m

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *_strbrk - check the code
+ *
+ * @s : char
+ *
+ * @accept : char
+ *
+ * Return:pointer
+ */
+char _strbrk(char *s, char *accept)
+{
+int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
