1388343ae06c871388e922bd4add30d8bb9cae35
crucialjun <nich.otieno@gmail.com>
2022-09-26 10:01:45 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *_memset - check the code
+ *
+ * @s : char
+ *
+ * @n : bytes
+ *
+ * @b : constant
+ * Return:.pointer
+ */
+char *_memset(char *s, char b,unsigned int n )
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
+}
