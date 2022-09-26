9f5db545f272ba358750c53f933fa2822d364776
crucialjun <nich.otieno@gmail.com>
2022-09-26 10:58:40 -0700

    m

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,7 +10,7 @@
  * @b : constant
  * Return:.pointer
  */
-char *_memset(char *s, char b,unsigned int n )
+char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
 for (i = 0; i < n; i++)
