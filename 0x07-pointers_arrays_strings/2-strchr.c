85e0dc495d08abfaae7781f5cd7aca8c02ea2089
crucialjun <nich.otieno@gmail.com>
2022-09-26 12:31:29 -0700

    m

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *_strchr - check the code
+ *
+ * @s : char
+ *
+ * @c : char
+ *
+ * Return:.pointer
+ */
+char *_strchr(char *s, char c)
+{
+while (*s)
+{
+if (*s != c)
+{
+s++;
+}
+else
+{
+return (s);
+}
+}
+
+if (c == '\0')
+{
+return (s);
+}
+
+return (NULL);
+}
