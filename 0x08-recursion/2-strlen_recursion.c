11434fee9e4c3a5b85789b73467025981fb1b0bb
crucialjun <nich.otieno@gmail.com>
2022-09-29 03:20:02 -0700

    m

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen_recursion - check the code
+ *
+ * @s : char
+ *
+ * Return:int
+ */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return (0);
+}
+return (1 + _strlen_recursion(s + 1));
+}
