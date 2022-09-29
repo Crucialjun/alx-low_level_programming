14c2a46a7084d60b1d58bf1d21dc2dba0684c4da
crucialjun <nich.otieno@gmail.com>
2022-09-29 04:45:00 -0700

    m

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _pow_recursion  - check code
+ * @x: int
+ * @y: int
+ *
+ * Return:  int
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
