218defb76cf75f2b9316c683f625b2e644aa8149
Crucialjun <nich.otieno@gmail.com>
2022-10-06 21:23:15 +0300

    checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - check code
+ * @b:  int
+ *
+ * Return: int
+ */
+void *malloc_checked(unsigned int b)
+{
+    void *p;
+
+    p = malloc(b);
+    if (p == NULL)
+        exit(98);
+    return (p);
+}
